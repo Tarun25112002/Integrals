54ebe847e4a5218154e0c1b82efc980115887922
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-09-21 17:09:41 +0530

    Fix routing path for educator dashboard and update Navbar and Educator components structure

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import {Outlet} from 'react-router-dom'
 const Educator = () => {
   return (
     <div>
-      <h1>Educator</h1>
+      <h1>Heading</h1>
+      <div><Outlet/></div>
+      <h1>Footer</h1>
     </div>
+
   );
 }
 
