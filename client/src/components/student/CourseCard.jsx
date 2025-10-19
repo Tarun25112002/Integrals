9469fe9ee5742f46b2a0beb77c585dc03d03fac5
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-10-19 17:54:36 +0530

    feat: Update CourseCard to display educator name directly; enhance CourseDetails with loading state and course rating display

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -20,7 +20,7 @@ const CourseCard = ({ course }) => {
         <h3 className="text-lg font-semibold text-gray-800 mb-2">
           {course.courseTitle}
         </h3>
-        <p className="text-gray-600 text-sm mb-3">{course.educator.name}</p>
+        <p className="text-gray-600 text-sm mb-3">Tarun Kumar Jha</p>
 
         <div className="flex items-center justify-between mb-3">
           <div className="flex items-center gap-2">
--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom";
 import { useState, useContext, useEffect } from "react";
 import { AppContext } from "../../context/AppContext";
+import Loading from "../../components/student/Loading";
+import { assets } from "../../assets/assets";
+
 
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
-  const { allCourses } = useContext(AppContext);
+  const { allCourses, calculateRating } = useContext(AppContext);
   const fetchCourseData = async () => {
     const findCourse = allCourses.find((course) => course._id === id);
     setCourseData(findCourse);
@@ -16,14 +19,49 @@ const CourseDetails = () => {
       fetchCourseData();
     }
   }, [allCourses, id]);
-  return (
-    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
-      <div className="absolute top-0 left-0 w-full h-[500px] z-0 bg-gradient-to-b from-cyan-100/70 to-transparent "></div>
-      {/* left column */}
-      <div></div>
-      {/* right column */}
-      <div></div>
-    </div>
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+        <div className="absolute top-0 left-0 w-full h-[500px] z-0 bg-gradient-to-b from-cyan-100/70 to-transparent "></div>
+        {/* left column */}
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800">
+            {courseData.courseTitle}
+          </h1>
+          <p className="pt-4 md:text-base text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+           <div className="flex items-center justify-between mb-3">
+                    <div className="flex items-center gap-2">
+                      <p className="text-sm font-medium text-gray-700">
+                        {calculateRating(courseData)}
+                      </p>
+                      <div className="flex">
+                        {[...Array(5)].map((_, i) => (
+                          <img
+                            key={i}
+                            src={
+                              i < Math.floor(calculateRating(courseData))
+                                ? assets.star
+                                : assets.star_blank
+                            }
+                            alt="star"
+                            className="w-4 h-4"
+                          />
+                        ))}
+                      </div>
+                    </div>
+                    <p className="text-sm text-gray-500">{courseData.courseRatings.length}{courseData.courseRatings.length > 1 ? " Ratings" : " Rating"}</p>
+                  </div>
+        </div>
+        {/* right column */}
+        <div></div>
+      </div>
+    </>
+  ) : (
+    <Loading />
   );
 };
 
