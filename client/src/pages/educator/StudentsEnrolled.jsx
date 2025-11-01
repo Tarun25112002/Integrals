f7e153e8bcbbe61d4ec57bad9efdef8172b8d3db
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-11-02 03:34:38 +0530

    feat(studentsEnrolled): Implement loading state for enrolled students

--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
 
+import { useEffect, useState } from "react";
+
+import { dummyStudentEnrolled } from "../../assets/assets";
+import Loading from "../../components/student/Loading";
 const StudentsEnrolled = () => {
-  return (
+const [studentEnrolled, setStudentEnrolled] = useState(null);
+const fetchEnrolledStudents = async () => {
+  setStudentEnrolled(dummyStudentEnrolled);
+}
+useEffect(() => {
+  fetchEnrolledStudents();
+}, []);
+  return studentEnrolled ? (
     <div>
       <h1>Students enrolled</h1>
     </div>
-  );
+  ):<Loading />
 }
 
 export default StudentsEnrolled
