935db520c88a624ca34dbea27422e5c7be5491a1
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-11-08 09:51:49 +0530

    import clerkWebhookSecret

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./configs/mongodb.js";
 import cors from "cors";
 import "dotenv/config";
+import clerkWebhookSecret from "./controllers/webhooks.js";
 import { connect } from "mongoose";
 const app = express();
 app.use(cors());
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req,res)=>{
     res.send("API Working")
 })
+app.post('/clerk', express.json(), clerkWebhookSecret)
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
