d59b42a06b060a7669e60db53a01353f3181086e
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-11-09 00:10:03 +0530

    updateRoleToEducator

---
+++ server/controllers/educatorController.js
@@ -0,0 +1,11 @@
+export const updateRoleToEducator = async (req, res)=>{
+    try{
+        const userId = req.auth.userId;
+        await clerkClient.users.updateUserMetadata(userId, {
+            publicMetadata: {role: 'educator'}
+        });
+       res.json({message: 'Role updated to educator successfully'});    
+    }catch(error){
+        res.json({message: 'Error updating role', error: error.message});
+    }
+}
