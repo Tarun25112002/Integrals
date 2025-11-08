9f5d1672b1339bac2b4ebec324a2532aa97eead1
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-11-08 09:42:18 +0530

    feat(server): restructure server initialization and add MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-
+import express from "express";
+import connectDB from "./configs/mongodb.js";
+import cors from "cors";
+import "dotenv/config";
+import { connect } from "mongoose";
 const app = express();
-
-app.use(cors())
+app.use(cors());
+await connectDB();
+const PORT = process.env.PORT || 5000;
+app.get('/', (req,res)=>{
+    res.send("API Working")
+})
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
